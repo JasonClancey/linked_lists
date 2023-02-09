b88dc9cd90e3f7d2d6859f541e06d3592d6da979
Jason Clancey <jason.clancey@dal.ca>
2023-02-08 21:12:08 -0700

    added 'to_s' method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -99,27 +99,25 @@ class LinkedList
         present_node = @head
         until present_node.nil?
             return index if present_node.value.eql?(value)
+
             index += 1
             present_node = present_node.next_node
         end
         nil
     end
 
+    def to_s
+        return if @head.nil?
+
+        present_node = @head
+        string = ''
+        until present_node.nil?
+            string += "( #{present_node.value} ) -> " unless present_node.nil?
+            present_node = present_node.next_node
+        end
+        string += 'nil'
+        puts string
+    end
+
 end
 
-list = LinkedList.new
-list.prepend(4)
-list.prepend(3)
-list.prepend(2)
-list.prepend(1)
-list.append(5)
-puts list.size
-puts list.tail.value
-puts list.at(0).value
-puts list.at(-2).value
-puts list.pop.value
-puts list.tail.value
-puts list.contains?(4)
-puts list.contains?("lol")
-puts list.find(1)
-p list.find(5)
